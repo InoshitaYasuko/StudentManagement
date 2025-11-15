42ba0898dfa006fc2fea10dedd86343c49626d1b
Inoshita <l1504033859as@gmail.com>
2025-11-15 14:57:15 +0900

    第５回スタンダード講義_練習用

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,17 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  private String name = "Inoshita Yasuko";
+  private String age = "32";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-  @GetMapping("/welcome/{name}")
-  public String saywelcome(@PathVariable String name) {
-    return "ようこそ、"+ name + "様";
+  @GetMapping("/studentInfo")
+  public String getstdentInfo() {
+    return name + " " + age + "歳";
+  }
+  @PostMapping("/studentInfo")
+  public void setstudentInfo(String name,String age) {
+    this.name = name;
+    this.age = age;
+  }
+  @PostMapping("/studentName")
+  public void updatsStundentName(String nama){
+    this.name = nama;
   }
 }
