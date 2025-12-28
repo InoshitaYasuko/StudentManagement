2a1aedf81b2cf064d77426214f51d71bae6c4ab7
Inoshita <l1504033859as@gmail.com>
2025-12-28 14:09:36 +0900

    Javaスタンダード第10回演習課題 students_coursesのオブジェクトを作成

---
+++ src/main/java/StudentCourse.java
@@ -0,0 +1,17 @@
+import lombok.Getter;
+import lombok.Setter;
+
+public class StudentCourse {
+
+  @Getter
+  @Setter
+
+  public class StudentCourse {
+
+    private String id;
+    private String studentId;
+    private String courseName;
+    private String startDate;
+    private String endDate;
+  }
+}
