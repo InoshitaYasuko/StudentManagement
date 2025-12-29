4f68318eb4c90fc532568fbf7b3a649861e72b64
Inoshita <l1504033859as@gmail.com>
2025-12-29 11:27:23 +0900

    Javaスタンダード第20回講義 講義狩猟

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -6,9 +6,19 @@ import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 
+/**
+ * 受講生情報を扱うリポジトリ。
+ *
+ * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
+ */
 @Mapper
 public interface StudentRepository {
 
+  /**
+   * 全件検索します。
+   *
+   * @return 全件検索した受講生情報の一覧
+   */
   @Select("SELECT * FROM students")
   List<Student> search();
 
