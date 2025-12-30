ab552ee9bbb66522f265905251186de070053663
Inoshita <l1504033859as@gmail.com>
2025-12-30 14:03:10 +0900

    Javaスタンダード第12回演習課題 修正済み

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -8,13 +8,15 @@ import lombok.Setter;
 @Setter
 
 public class Student {
-   private String id;
-   private String fullName;
-   private String furigana;
-   private String nickname;
-   private String email;
-   private String city;
-   private int age;
-   private String gender;
-   private String remark;
-   private boolean isDeleted;
+
+  private String id;
+  private String fullName;
+  private String furigana;
+  private String nickname;
+  private String email;
+  private String city;
+  private int age;
+  private String gender;
+  private String remark;
+  private boolean isDeleted;
+}
