e9ca481eb261feaeaf8524d5c64c23df1f753f81
Inoshita <l1504033859as@gmail.com>
2026-01-10 14:58:36 +0900

    Javaスタンダード第25回講義

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -3,10 +3,14 @@ package raisetech.StudentManagement.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.naming.Binding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.conveter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
@@ -19,6 +23,7 @@ public class StudentController {
 
   private StudentService service;
   private StudentConverter converter;
+
   @Autowired
   public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
@@ -30,12 +35,23 @@ public class StudentController {
     List<Student> students = service.searchStudentList();
     List<StudentCourse> studentCourses = service.searchStudentCourseList();
 
-
-    modul.addAttribute("studentList" ,converter.convertStudentDetails(students, studentCourses));
+    modul.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
     return "studentList";
   }
+
   @GetMapping("/studentCourseList")
   public List<StudentCourse> getStudentCourseList() {
     return service.searchStudentCourseList();
   }
 }
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    return "";
+  }
+
