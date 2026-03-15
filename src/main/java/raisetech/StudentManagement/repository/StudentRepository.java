3dd93a56c43f62f47e694e21aad5066bba3b788b
Inoshita <l1504033859as@gmail.com>
2026-03-15 15:33:17 +0900

    Javaスタンダード第39回講義_mapperXMLの追加

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -14,101 +14,13 @@ import raisetech.StudentManagement.data.StudentCourse;
  */
 @Mapper
 public interface StudentRepository {
-
-  /**
-   * 受講生の全件検索を行います。
-   *
-   * @return　受講生一覧(全件)
-   */
-  @Select("SELECT * FROM students WHERE is_deleted= 0")
   List<Student> search();
-
-  /**
-   * 受講生の検索を行います。
-   *
-   * @param id　受講生ID
-   * @return　受講生
-   */
-  @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
-
-  /**
-   * 受講生のコース情報の全件検索を行います。
-   *
-   * @return　受講生のコース情報(全件)
-   */
-  @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchCourseList();
-
-  /**
-   * 受講生IDに紐づく受講生コース情報を検索します。
-   *
-   * @param id　受講生ID
-   * @return 受講生IDに紐づく受講生コース情報
-   */
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  StudentCourse searchStudentCourse(String id);
-
-  @Select("SELECT * FROM students WHERE id = #{id}")
   Student findStudentById(int id);
-
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentCourse> findStudentCourseByStudentId(int studentId);
-
-  /***
-   * 受講生を新規登録します。
-   * IDに関しては自動採番を行う。
-   *
-   * @param student　受講生
-   */
-  @Insert(
-      "INSERT INTO students (full_name, furigana, nickname, email, city, age, gender, remark,is_deleted)"
-          + " VALUES (#{fullName}, #{furigana}, #{nickname}, #{email}, #{city}, #{age}, #{gender}, #{remark},#{isDeleted})")
-  @Options(useGeneratedKeys = true, keyProperty = "id")
   void insertStudent(Student student);
-
-  /***
-   * 受講生コース情報を新規登録します。
-   * IDに関しては自動採番を行う。
-   *
-   * @param studentCourse 受講生コース情報
-   */
-  @Insert("INSERT INTO students_courses (student_id,course_name,start_date,end_date)"
-      + " VALUES (#{studentId},#{courseName},#{startDate},#{endDate})")
   void insertStudentCourse(StudentCourse studentCourse);
-
-  /***
-   * 受講生を更新します。
-   *
-   * @param student　受講生
-   */
-  @Update("""
-      UPDATE students 
-      SET full_name = #{fullName},
-      furigana = #{furigana},
-      nickname = #{nickname},
-      email = #{email},
-      city = #{city},
-      age = #{age},
-      gender = #{gender},
-      remark = #{remark},
-      is_deleted = #{isDeleted}
-      WHERE id = #{id}
-      """)
   void updateStudent(Student student);
-
-  /***
-   * 受講生コース情報のコース名を更新します。
-   *
-   * @param studentCourse　受講生コース情報
-   */
-  @Update("""
-            UPDATE students_courses
-            SET
-            course_name = #{courseName},
-            start_date = #{startDate},
-            end_date = #{endDate}
-            WHERE id = #{id}
-      """)
   void updateStudentCourse(StudentCourse studentCourse);
 }
