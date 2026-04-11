6212d7e39e863076d453f2ca5ca87dc4f86ef2bb
Inoshita <l1504033859as@gmail.com>
2026-04-11 14:49:36 +0900

    Javaスタンダード第27回演習課題_受講生登録_動作確認済

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -6,12 +6,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.StudentManagement.controller.conveter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
@@ -31,15 +29,15 @@ class StudentServiceTest {
   private StudentService sut;
 
   @BeforeEach
-  void before(){
+  void before() {
     sut = new StudentService(repository, converter);
   }
 
   @Test
-  void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せている事(){
+  void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せている事() {
     StudentService sut = new StudentService(repository, converter);
     List<Student> studentList = new ArrayList<>();
-    List<StudentCourse>  studentCourseList = new ArrayList<>();
+    List<StudentCourse> studentCourseList = new ArrayList<>();
     List<StudentDetail> expected = new ArrayList<>();
 
     when(repository.search()).thenReturn(studentList);
@@ -52,5 +50,20 @@ class StudentServiceTest {
     verify(repository, times(1)).search();
     verify(repository, times(1)).searchCourseList();
     verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
+  }
+  @Test
+  void 受講生登録_リポジトリの処理が適切に呼び出せている事() {
+    Student student = new Student();
+    student.setId(1);
+    StudentCourse course = new StudentCourse();
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentCourseList(List.of(course));
+
+    sut.registerStudent(studentDetail);
+
+    verify(repository).insertStudent(student);
+    verify(repository).insertStudentCourse(course);
   }
 }
