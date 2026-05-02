537a4dfee437843804f8a1ddfcb3c03aea019e42
Inoshita <l1504033859as@gmail.com>
2026-05-02 15:16:02 +0900

    Javaスタンダード第29回演習課題_受講生情報とコース情報が正しく紐づくこと

--- src/test/java/raisetech/StudentManagement/controller/conveter/StudentConverterTest.java
+++ src/test/java/raisetech/StudentManagement/controller/conveter/StudentConverterTest.java
@@ -2,11 +2,33 @@ package raisetech.StudentManagement.controller.conveter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
 
 class StudentConverterTest {
 
   private StudentConverter converter = new StudentConverter();
 
   @Test
+  void 受講生情報とコース情報が正しく紐づくこと(){
+    Student student = new Student();
+    student.setId("1");
+
+    StudentCourse course = new StudentCourse();
+    course.setStudentId("1");
+
+    List<StudentDetail> result = converter.convertStudentDetails(
+        List.of(student),
+        List.of(course)
+    );
+
+    assertEquals(1, result.size());
+
+    StudentDetail detail = result.get(0);
+    assertEquals(student, detail.getStudent());
+    assertEquals(1, detail.getStudentCourseList().size());
+  }
 }
