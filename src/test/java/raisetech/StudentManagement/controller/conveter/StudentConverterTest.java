412fc8235ad31c867131ab2181eec5b7f5a481b5
Inoshita <l1504033859as@gmail.com>
2026-05-02 15:00:07 +0900

    Javaスタンダード第29回演習課題_テストクラス作成

---
+++ src/test/java/raisetech/StudentManagement/controller/conveter/StudentConverterTest.java
@@ -0,0 +1,12 @@
+package raisetech.StudentManagement.controller.conveter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StudentConverterTest {
+
+  private StudentConverter converter = new StudentConverter();
+
+  @Test
+}
