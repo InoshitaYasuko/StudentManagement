b24a87482859065ef014d901dff6ff9ef2c3094c
Inoshita <l1504033859as@gmail.com>
2026-05-02 15:23:57 +0900

    Javaスタンダード第29回演習課題_複数のコースが紐づくこと

--- src/test/java/raisetech/StudentManagement/controller/conveter/StudentConverterTest.java
+++ src/test/java/raisetech/StudentManagement/controller/conveter/StudentConverterTest.java
@@ -46,4 +46,21 @@ class StudentConverterTest {
 
     assertEquals(0, result.get(0).getStudentCourseList().size());
   }
-}
+  @Test
+  void 複数のコースが紐づくこと(){
+    Student student = new Student();
+    student.setId("1");
+
+    StudentCourse course1 = new StudentCourse();
+    course1.setStudentId("1");
+
+    StudentCourse course2 = new StudentCourse();
+    course2.setStudentId("1");
+
+    List<StudentDetail> result = converter.convertStudentDetails(
+        List.of(student),
+        List.of(course1, course2)
+    );
+
+    assertEquals(2, result.get(0).getStudentCourseList().size());
+  }
